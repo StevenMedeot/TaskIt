ff58332ffc6b7f1c66c5fd15e712deb05dc93a0d
StevenMedeot <55761357+StevenMedeot@users.noreply.github.com>
2020-01-15 16:45:24 -0500

    Revert "Revert "Fragment and Navigation setup""
    This reverts commit ffd7af19ac98e17fd5cdd200b713426fbd3bb1bd.

--- app/src/main/java/com/mash/taskit/MainActivity.java
+++ app/src/main/java/com/mash/taskit/MainActivity.java
@@ -1,14 +1,60 @@
 package com.mash.taskit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.mash.taskit.screens.HomeFragment;
+import com.mash.taskit.screens.TaskManagerFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+
+        Button homeButton = findViewById(R.id.mainNav);
+        homeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //Toast.makeText(getApplicationContext(), "Map", Toast.LENGTH_SHORT).show();
+                Fragment fragment = new HomeFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame, fragment); // replace a Fragment with Frame Layout
+                transaction.commit(); // commit the changes
+            }
+        });
+
+        Button taskManagerButton = findViewById(R.id.taskNav);
+        taskManagerButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //Toast.makeText(getApplicationContext(), "Map", Toast.LENGTH_SHORT).show();
+                Fragment fragment = new TaskManagerFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame, fragment); // replace a Fragment with Frame Layout
+                transaction.commit(); // commit the changes
+            }
+        });
+
+
+
     }
 }
+
+
